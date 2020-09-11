300521c772f27ef2e194f2bf9e03cb849e0ffae7
Kakk22 <1029839308@qq.com>
2020-09-11 20:48:39 +0800

    移动0 数组 双指针解法

--- array/src/com/cyf/array/MoveZeroes.java
+++ array/src/com/cyf/array/MoveZeroes.java
@@ -14,9 +14,44 @@ package com.cyf.array;
 //
 // Related Topics 数组 双指针
 // 👍 729 👎 0
+
+import java.util.Arrays;
+
 /**
  * @author by cyf
  * @date 2020/9/11.
  */
 public class MoveZeroes {
+
+    /**
+     * 解题思路 快慢指针
+     * 当遇到0时，则快指针继续往下走，直到遇到0 则把交换快慢指针所指向的值，慢指针会一直指向0
+     * 这样就会把所有的0往数组后面挪
+     * @param arr
+     * @return
+     */
+    public static int[] moveZeroes(int arr []){
+        if (arr.length < 0){
+            throw  new RuntimeException("数组长度必须大于0");
+        }
+        int zero = 0;
+        int j = 0 ;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == zero){
+                continue;
+            }
+            //如果不等于0 则交换
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+            j++;
+        }
+        return  arr;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {0,1,0,3,0,0,0,2,3,12};
+        int[] ints = MoveZeroes.moveZeroes(arr);
+        System.out.println(Arrays.toString(ints));
+    }
 }
