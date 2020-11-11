0ef3d789bd268a9d0b0b371829440e64af7f66ce
Kakk22 <1029839308@qq.com>
2020-11-11 23:27:04 +0800

    两数之和    lru

---
+++ array/src/com/cyf/list/addTowNumbers.java
@@ -0,0 +1,87 @@
+package com.cyf.list;
+//给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+//
+// 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+//
+// 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+//
+// 示例：
+//
+// 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+//输出：7 -> 0 -> 8
+//原因：342 + 465 = 807
+//
+// Related Topics 链表 数学
+// 👍 5236 👎 0
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/**
+ * [9]
+ * [1,9,9,9,9,9,9,9,9,9]
+ *
+ * @author by cyf
+ * @date 2020/11/11.
+ */
+public class addTowNumbers {
+
+    public static void main(String[] args) {
+        ListNode first = new ListNode(8, new ListNode(4, new ListNode(8)));
+        ListNode second = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode listNode = addTwoNumbers(first, second);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+            if (l1 != null){
+                l1 = l1.next;
+            }
+            if (l2 != null){
+                l2 = l2.next;
+            }
+            if (carry == 1){
+                cur.next = new ListNode(carry);
+            }
+        }
+        return pre.next;
+    }
+
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
